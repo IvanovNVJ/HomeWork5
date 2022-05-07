c6ada818731d5115adb8c22b76ff37f75953564a
Ivanov Nikita <ivanovnikita11111001110@gmail.com>
2022-05-07 12:41:01 +0700

    Добавьте файлы проекта.

---
+++ HomeWork5.1/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeWork5._1
+{
+    internal class Program
+    {
+        static string[] StringSplitFunction(string sentence)
+        {
+             return sentence.Split(' '); 
+                   
+        }
+        static void Print (string [] Array)
+        {
+            for (int i = 0; i < Array.Length; i++)
+            {
+                Console.WriteLine (Array [i]);
+                            }
+        }
+        static void Main()
+        {
+            Console.WriteLine("Введите предложение");
+            string arr1 = Console.ReadLine();
+            string[] arr2 = StringSplitFunction(arr1);
+            Print(arr2);
+            Console.ReadKey();
+        }
+    }
+}
---
+++ HomeWork5.2/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeWork5._2
+{
+    internal class Program
+    {
+        static void Main()
+        {
+
+        }
+    }
+}
